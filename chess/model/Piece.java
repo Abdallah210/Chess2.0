ead78b1e40f6d8c44c96fcff413c62d831933fe4
Elfilali210 <abdallah.elfilali.etu@univ-lille.fr>
2024-07-13 17:08:01 +0200

    add attributes, getters and setters in Piece class

--- chess/model/Piece.java
+++ chess/model/Piece.java
@@ -1,5 +1,56 @@
 package chess.model;
 
+import chess.utils.Point;
+
 public abstract class Piece {
-    // TO DO
+
+    protected Point point;
+    protected boolean isWhite;
+    protected String type;
+    protected boolean hasMoved;
+
+    public Piece(Point point, boolean isWhite, String type) {
+        this.point.setX(point.getX());
+        this.point.setY(point.getY());
+        this.isWhite = isWhite;
+        this.type = type;
+        this.hasMoved = false;
+    }
+
+    public int getX() {
+        return this.point.getX();
+    }
+
+    public void setX(int x) {
+        this.point.setX(x);
+    }
+
+    public int getY() {
+        return this.point.getY();
+    }
+
+    public void setY(int y) {
+        this.point.setY(y);
+    }
+
+    public boolean isWhite() {
+        return this.isWhite;
+    }
+
+    public void setWhite(boolean white) {
+        this.isWhite = white;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public boolean hasMoved() {
+        return this.hasMoved;
+    }
+
+    public void setMoved(boolean hasMoved) {
+        this.hasMoved = hasMoved;
+    }
+
 }
