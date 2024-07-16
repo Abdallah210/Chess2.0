62846f21c1f599f536976412441c63ddab03c70f
Elfilali210 <abdallah.elfilali.etu@univ-lille.fr>
2024-07-16 12:42:26 +0200

    Implement Board and Cell classes

--- chess/model/Board.java
+++ chess/model/Board.java
@@ -1,5 +1,29 @@
 package chess.model;
 
+import chess.utils.Point;
+
 public  class Board {
-    // TO DO
+    
+    private Cell[][] cells =  new Cell[8][8];
+
+    public Board(){
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Point point = new Point(i, j);
+                if (point.isPair()) {
+                    this.cells[i][j] = new Cell(point, true);
+                } else {
+                    this.cells[i][j] = new Cell(point, false);                
+                }
+            }        
+        }
+
+    }
+
+
+
+
+
+
 }
